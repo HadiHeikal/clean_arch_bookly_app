193e1bc8a939440199dc86b876a4c3b475eaea9a
HadiHeikal <hadiiheikal@gmail.com>
2026-06-17 15:44:45 +0300

    home repo

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String bookImage;
+  final String bookTitle;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.bookImage, this.bookTitle, this.authorName, this.price, this.rating);
+}
