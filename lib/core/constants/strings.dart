ab274b578c9140ea4189aae2319fd87ed0cc5f7f
HadiHeikal <hadiiheikal@gmail.com>
2026-06-22 16:09:31 +0300

    feat: add features endpoints

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,20 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 final String baseUrl = "https://www.googleapis.com/books/v1/";
 final String apiKey = dotenv.env['BOOKS_API_KEY'] ?? "";
+
+final String searchFreeNewestBooksEndpoint =
+    "/volumes?Filtering=free-ebooks&sorting=newest&q=programming&key=$apiKey";
+
+final String searchFeaturedBooksEndpoint =
+    "/volumes?Filtering=free-ebooks&q=programming&key=$apiKey";
+
+final String searchBestSellerBooksEndpoint =
+    "/volumes?Filtering=free-ebooks&orderBy=relevance&q=fiction&key=$apiKey";
+
+final String searchTopRatedBooksEndpoint =
+    "/volumes?Filtering=free-ebooks&maxResults=40&q=subject:science&key=$apiKey";
+
+String searchSimilarBooksEndpoint(String query, {int maxResults = 10}) {
+  final encodedQuery = Uri.encodeQueryComponent(query);
+  return "/volumes?Filtering=free-ebooks&orderBy=relevance&maxResults=$maxResults&q=$encodedQuery&key=$apiKey";
+}
