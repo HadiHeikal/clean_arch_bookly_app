67d239f69e3004de5921621604dbdb07d698b78b
HadiHeikal <hadiiheikal@gmail.com>
2026-07-11 16:18:12 +0300

    generate books_entity adapter

--- lib/Features/home/domain/use_cases/use_cases.dart
+++ lib/Features/home/domain/use_cases/use_cases.dart
@@ -2,7 +2,8 @@ import 'package:dartz_plus/dartz_plus.dart';
 
 import '../../../../core/errors/failure.dart';
 
-abstract class UseCase<T, Param> {
+// Generic class
+abstract class FetchBooksUseCase<T, Param> {
   Future<Either<Failure,T>> call([Param? param]);
 }
 
