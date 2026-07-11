07f35a8f24a352f78adc053f2a4c6d227cd24530
HadiHeikal <hadiiheikal@gmail.com>
2026-07-11 16:23:45 +0300

    generate books_entity adapter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,10 +1,19 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity extends Equatable {
+  @HiveField(0)
   final String bookImage;
+  @HiveField(1)
   final String bookTitle;
+  @HiveField(2)
   final String bookId;
+  @HiveField(3)
   final String authorName;
+  @HiveField(4)
   final num price;
+  @HiveField(5)
   final num rating;
 
 
