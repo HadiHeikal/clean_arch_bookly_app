b146a7691f9937726943883db4b186b1bf1c1305
HadiHeikal <hadiiheikal@gmail.com>
2026-07-11 18:39:03 +0300

    helper functions for caching

--- lib/core/functions/books_box_cache_data.dart
+++ lib/core/functions/books_box_cache_data.dart
@@ -4,7 +4,15 @@ import 'package:hive/hive.dart';
 import '../../Features/home/domain/entities/book_entity.dart';
 
 // cache the List of books entity at booksBox [Hive]
-void saveDataAtBooksBox({required List<BookEntity> booksList,required String kFeaturedBox }){
-  var booksBox = Hive.box(kFeaturedBox);
+void saveDataAtBooksBox({required List<BookEntity> booksList,required String boxName }){
+  var booksBox = Hive.box(boxName);
   booksBox.addAll(booksList);
 }
+
+// retrieve the List of books entity from booksBox by box name [Hive]
+List<BookEntity> getDataFromBooksBox({required String boxName }){
+  var booksBox = Hive.box<BookEntity>(boxName);
+  List<BookEntity> listOfFeaturedBooks = booksBox.values.toList();
+  return listOfFeaturedBooks;
+}
+
