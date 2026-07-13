2c4b50ae2b06afae78e8cd05b6932df2f20e5bcc
HadiHeikal <hadiiheikal@gmail.com>
2026-07-13 12:34:50 +0300

    feat : create featured books cubit

--- lib/Features/home/presentation/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,27 @@
 import 'package:bloc/bloc.dart';
-import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:meta/meta.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:dartz_plus/dartz_plus.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase ;
+  FeaturedBooksCubit({required this.featuredBooksUseCase}) : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBook() async{
+    // emit loading state
+    emit(FeaturedBooksLoading());
+
+    // call the featuredBooksUseCase and save the result at a variable [List<BookEntity>] or [Failure]
+    var result = featuredBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.message));
+      },
+      (booksList) {
+        emit(FeaturedBooksSuccess(booksList));
+      },
+    );
+  }
 }
