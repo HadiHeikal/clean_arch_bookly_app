5dcc9ce61634b310a314abf2fb2613208416494d
HadiHeikal <hadiiheikal@gmail.com>
2026-07-14 17:07:58 +0300

    refactor : make custom book items list receives the Image as a String, add a CachedNetworkImage instead of Container

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assets.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  final String?image;
+  const CustomBookImage({super.key, this.image});
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                AssetsData.testImage,
-              ),
-            )),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16), // Gives a clean modern look to the book cover
+        child: CachedNetworkImage(
+          imageUrl: image ?? '',
+          fit: BoxFit.fill,
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(), // Loading state
+          ),
+          errorWidget: (context, url, error) => const Center(
+            child: Icon(
+              Icons.error_outline,
+              size: 30,
+              color: Colors.red, // Error state if image fails to load or URL is empty
+            ),
+          ),
+        ),
       ),
     );
   }
